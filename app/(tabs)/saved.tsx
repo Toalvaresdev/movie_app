fda9ae7ea4830621670a92a2f95dce8cf3eb3b9c
Toalvaresdev <tomasalvares0237@gmail.com>
2025-03-11 23:38:35 -0300

    Iniciando app de peliculas en react native

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const saved = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-accent font-bold">Guardados!</Text>
+
+    </View>
+  )
+}
+
+export default saved
